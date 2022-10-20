7a1ab49cc4c71bd0a2ef632de745abde287b942e
ytavz <ytavz@DESKTOP-DAAEB8D>
2022-10-20 19:40:12 +0300

    Add project files.

---
+++ oop 1/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_1
+{
+    class Person
+    {
+        public string fName;
+        public string lName;
+        public int age;
+
+        public void Print()
+        {
+            Console.WriteLine($"age: {age}");
+            Console.WriteLine("\n");
+            if (fName.Length > 5)
+            {
+                Console.WriteLine($"{fName}");
+                Console.WriteLine($"Your name consists of {fName.Length} Letters");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
---
+++ oop 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace oop_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ps = new Person();
+            Console.WriteLine("Enter a name");
+            ps.fName = Console.ReadLine();
+            Console.WriteLine("Enter an age");
+            ps.age = int.Parse(Console.ReadLine());
+
+            ps.Print();
+
+        }
+    }
+}
